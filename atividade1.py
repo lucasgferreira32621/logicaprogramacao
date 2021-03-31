eb4e09fcbd38ca8dc6a645a9117a452c808080e7
lucasgferreira32621 <81182737+lucasgferreira32621@users.noreply.github.com>
2021-03-31 17:03:17 -0300

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -1,1 +1,41 @@
-  print ("ola Professor")
+soma = 0
+lista = [1, 1]
+def calcula_soma(i):
+    i = numeros
+for soma in lista: 
+    soma += int(soma)
+
+print(soma) 
+
+
+
+
+i = "3 1 6 7 9"
+numeros = i. split()
+lista = [] 
+def converte_entrada(numeros): 
+    for numero in numeros:
+
+        lista.append(int (numero))
+    return lista
+lista = converte_entrada(numeros)
+print(lista)
+i = [1, 3, 6, 8]
+val1 = 0 
+val2 =  len(i) 
+def processa_numeros (numeros):
+    i = numeros
+for numeros in i: 
+    val1 += int(numeros)
+print(val1)
+print(val2)
+
+def main(numeros):
+  i=soma_index[1]
+  soma=soma_index[0]
+  media=soma/i
+  return media
+lista=converte_entrada (input())
+soma_index=processar_numeros(lista)
+media=main(lista)
+print(media)
