6fa3a4f643f507bc5a4c1d71bc74c745737297e7
lucasgferreira32621 <81182737+lucasgferreira32621@users.noreply.github.com>
2021-04-13 14:07:28 -0300

    Update prova2.py

--- prova2.py
+++ prova2.py
@@ -1,15 +1,15 @@
-def exerc1():
+def media_aluno():
     nota1 = int(input("Digite a nota 1: "))
     nota2 = int(input("Digite a nota 2: "))
     nota3 = int(input("Digite a nota 3: "))
 
-    a_soma_notas = nota1 + nota2 + nota3
-    a_media_notas = a_soma_notas/3
+    soma_notas = nota1 + nota2 + nota3
+    media_notas = soma_notas/3
 
-    print(f'Média do aluno é {a_media_notas}')
+    print(f'Média do aluno é {media_notas}')
 
 
-a_media_aluno()
+media_aluno()
 
 def exerc2():
   nume_entradas = int(input("Digite o tamanho da lista: "))
